daf7e6f6a13d83855d79c6234d46b0503f50ab3c
newtom69 <thomas@vuille.fr>
2020-06-27 20:07:47 +0200

    93084

--- NarcissisticNumberTest/NarcissisticNumberListShould.cs
+++ NarcissisticNumberTest/NarcissisticNumberListShould.cs
@@ -23,5 +23,17 @@ namespace NarcissisticNumberDojoTest
         {
             Assert.Equal(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 153 }, NarcissisticNumber.NarcissisticNumberList(153));
         }
+
+        [Fact]
+        public void Return_1_to_9_153_370_when_370()
+        {
+            Assert.Equal(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370 }, NarcissisticNumber.NarcissisticNumberList(370));
+        }
+
+        [Fact]
+        public void Return_good_list_when_93084()
+        {
+            Assert.Equal(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407, 1634, 8208, 9474, 54748, 92727, 93084 }, NarcissisticNumber.NarcissisticNumberList(93084));
+        }
     }
 }
