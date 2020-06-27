4bfc8f50961a694a6b4dc4a4ee113e367df998b5
newtom69 <thomas@vuille.fr>
2020-06-27 17:55:49 +0200

    Ajoutez des fichiers projet.

---
+++ NarcissisticNumber/NarcissisticNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NarcissisticNumberDojo
+{
+    public class NarcissisticNumber
+    {
+        public static List<int> NarcissisticNumberList(int maxNumber)
+        {
+            List<int> narcissisticNumbers = new List<int>();
+
+
+            return narcissisticNumbers;
+        }
+    }
+}
